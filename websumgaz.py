7be68ad2e79d409cd38c0ac636136845f5f226ef
chetverovod <chetverovod@gmail.com>
2022-11-27 15:11:03 +0300

    Debug.

--- websumgaz.py
+++ websumgaz.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3 
+# -*- coding: utf-8 -*-
 
 import io
 import streamlit as st
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
-# from PIL import Image
-# import numpy as np
-# from tensorflow.keras.applications import EfficientNetB0
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from io import StringIO
 
 model_name = "IlyaGusev/rugpt3medium_sum_gazeta"
 
@@ -23,13 +19,19 @@ def load_txt():
     uploaded_file = st.file_uploader(label='Выберите текст для распознавания')
     if uploaded_file is not None:
         txt_data = uploaded_file.getvalue()
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
         #st.image(txt_data)
         #return Image.open(io.BytesIO(txt_data))
-        return st.text_area(txt_data)
+        #return st.text_area(txt_data)    
+        return string_data
     else:
         return None
-# ----------------------------------------------------------------------
-#article_text = "По информации издания, для того, чтобы продолжить работу в Twitter, сотрудникам нужно перейти по ссылке в письме и нажать «да» до 17:00 по местному времени (01:00 по московскому времени 17 ноября). В противном случае работников уволят с выплатой трёхмесячного выходного пособия.  В своём письме Маск не обещает работникам лёгкой жизни. По его словам, оставшийся персонал будет работать «долгими часами и с высокой интенсивностью». «Только исключительная продуктивность будет проходным баллом», — отметил Маск.  По данным издания Daily Mail, Маск, уже уволил не менее десяти сотрудников, которые критиковали его в корпоративном мессенджере Slack. Это сделало его «Тем-Кого-Нельзя-Называть», подобно Волан-де-Морту из саги о Гарри Поттере. Сотрудники боятся, что новые коллеги, которых Маск привёл с собой из Tesla, будут просматривать все сообщения в Slack и по поисковым запросам «Илон» и «Маск» находить людей, которые грубо высказываются о новом начальнике."
 
 def summarize(model, tokenizer, article_text):
     text_tokens = tokenizer(
@@ -43,7 +45,8 @@ def summarize(model, tokenizer, article_text):
 
     output_ids = model.generate(
     input_ids=input_ids,
-    no_repeat_ngram_size=4
+    no_repeat_ngram_size=4,
+    max_new_tokens = 100
     )
 
     summary = tokenizer.decode(output_ids[0], skip_special_tokens=False)
@@ -59,8 +62,12 @@ def print_summary(s):
 model, tokenizer = load_model()
 
 
-st.title('Генерация кратких выводов  из газетной статьи')
+st.title('Генерация кратких выводов из газетной статьи')
 txt = load_txt()
+
+#st.write('-------------------------')
+#st.write(txt)
+#st.write('-------------------------')
 result = st.button('Анализировать')
 if result:
     sum = summarize(model, tokenizer, txt)
