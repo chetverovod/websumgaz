26f1764c51bd673ea769c4c3045bd31fbce501f3
chetverovod <chetverovod@gmail.com>
2022-11-27 15:53:04 +0300

    It works.

--- websumgaz.py
+++ websumgaz.py
@@ -11,14 +11,12 @@ model_name = "IlyaGusev/rugpt3medium_sum_gazeta"
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    # return EfficientNetB0(weights='imagenet')
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return  AutoModelForCausalLM.from_pretrained(model_name), tokenizer
 
 def load_txt():
     uploaded_file = st.file_uploader(label='Выберите текст для распознавания')
     if uploaded_file is not None:
-        txt_data = uploaded_file.getvalue()
         # To convert to a string based IO:
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         st.write(stringio)
@@ -26,9 +24,6 @@ def load_txt():
         # To read file as string:
         string_data = stringio.read()
         st.write(string_data)
-        #st.image(txt_data)
-        #return Image.open(io.BytesIO(txt_data))
-        #return st.text_area(txt_data)    
         return string_data
     else:
         return None
@@ -53,7 +48,6 @@ def summarize(model, tokenizer, article_text):
     summary = summary.split(tokenizer.sep_token)[1]
     summary = summary.split(tokenizer.eos_token)[0]
     return summary  
-#-----------------------------------------------------------------------
 
 def print_summary(s):
         st.write(s)
@@ -62,36 +56,13 @@ def print_summary(s):
 model, tokenizer = load_model()
 
 
-st.title('Генерация кратких выводов из газетной статьи')
+st.title('Генератор кратких выводов из газетной статьи')
 txt = load_txt()
 
-#st.write('-------------------------')
-#st.write(txt)
-#st.write('-------------------------')
-result = st.button('Анализировать')
-if result:
-    sum = summarize(model, tokenizer, txt)
-    st.write('**Выводы:**')
-    print_summary(sum)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if txt:
+    result = st.button('Анализировать')
+    if result:
+        sum = summarize(model, tokenizer, txt)
+        st.write('**Выводы:**')
+        print_summary(sum)
 
